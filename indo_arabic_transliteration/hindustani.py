5fa1d2f7152e5446281f9e49200fe57238edffe4
Gokul NC <gokulnc@ymail.com>
2021-04-10 23:27:00 +0530

    Urdu Abjad to Hindi fixes

--- indo_arabic_transliteration/hindustani.py
+++ indo_arabic_transliteration/hindustani.py
@@ -1,6 +1,6 @@
 ARABIC_NORMALIZE_MAP = {
-    'أ': 'ا',
-    'ۃ': "ت",
+    'أ': "ا",
+    'ۃ': "ہ",
 }
 arabic_normalizer = str.maketrans(ARABIC_NORMALIZE_MAP)
 
@@ -9,7 +9,13 @@ URDU_POSTPROCESS_MAP = {
 }
 urdu_postprocessor = str.maketrans(URDU_POSTPROCESS_MAP)
 
-HINDUSTANI_MAP_FILES = ['hindustani_consonants.csv', 'hindustani_initial_vowels.csv', 'hindustani_vowels.csv', 'hindustani_numerals.csv', 'hindustani_punctuations.csv']
+HINDI_TO_URDU_VOWELS_ABJAD = {
+    
+}
+
+INITIAL_HINDUSTANI_MAP_FILES = ['hindustani_initial_vowels.csv']
+HINDUSTANI_MAP_FILES = ['hindustani_consonants.csv', 'hindustani_vowels.csv', 'hindustani_numerals.csv', 'hindustani_punctuations.csv']
+FINAL_HINDUSTANI_MAP_FILES = ['hindustani_final_vowels.csv']
 
 import os
 import re
@@ -19,20 +25,33 @@ from .utils import StringTranslator
 class HindustaniTransliterator:
     def __init__(self):
         data_dir = os.path.dirname(__file__) + '/data/'
+        self.initial_urdu_to_hindi_map = {}
+        self.final_urdu_to_hindi_map = {}
         self.urdu_to_hindi_map = {}
+
+        for map_file in INITIAL_HINDUSTANI_MAP_FILES:
+            df = pd.read_csv(data_dir+map_file, header=None)
+            for i in df.columns:
+                urdu_letter, roman_letter, hindi_letter = str(df[i][0]).strip(), str(df[i][1]).strip(), str(df[i][2]).strip()
+                self.initial_urdu_to_hindi_map[urdu_letter] = hindi_letter
+        
+        for map_file in FINAL_HINDUSTANI_MAP_FILES:
+            df = pd.read_csv(data_dir+map_file, header=None)
+            for i in df.columns:
+                urdu_letter, roman_letter, hindi_letter = str(df[i][0]).strip(), str(df[i][1]).strip(), str(df[i][2]).strip()
+                self.final_urdu_to_hindi_map[urdu_letter] = hindi_letter
         
         for map_file in HINDUSTANI_MAP_FILES:
             df = pd.read_csv(data_dir+map_file, header=None)
             for i in df.columns:
-                self.urdu_to_hindi_map[str(df[i][0]).strip()] = str(df[i][2]).strip()
+                urdu_letter, roman_letter, hindi_letter = str(df[i][0]).strip(), str(df[i][1]).strip(), str(df[i][2]).strip()
+                self.urdu_to_hindi_map[urdu_letter] = hindi_letter
                 if 'consonants' in map_file:
                     # Non-initial forms: Consonant+ا to Consonant+ा
-                    self.urdu_to_hindi_map[str(df[i][0]).strip()+'ا'] = str(df[i][2]).strip()+'ा'
-                    # Final forms: Consonant+ی to Consonant+ी
-                    self.urdu_to_hindi_map[str(df[i][0]).strip()+'ی'+' '] = str(df[i][2]).strip()+'ी '
-                    # Final forms: Consonant+و to Consonant+ो (Approximation for readability)
-                    self.urdu_to_hindi_map[str(df[i][0]).strip()+'و'+' '] = str(df[i][2]).strip()+'ो '
+                    self.urdu_to_hindi_map[urdu_letter+'ا'] = hindi_letter+'ा'
 
+        self.initial_urdu_to_hindi_converter = StringTranslator(self.initial_urdu_to_hindi_map, match_initial_only=True)
+        self.final_urdu_to_hindi_converter = StringTranslator(self.final_urdu_to_hindi_map, match_final_only=True)
         self.urdu_to_hindi_converter = StringTranslator(self.urdu_to_hindi_map)
 
         from indicnlp.normalize.indic_normalize import IndicNormalizerFactory
@@ -45,6 +64,7 @@ class HindustaniTransliterator:
     
     def transliterate_from_urdu_to_hindi(self, text):
         text = self.normalize_urdu(text)
+        text = self.initial_urdu_to_hindi_converter.translate(text)
+        text = self.final_urdu_to_hindi_converter.translate(text)
         text = self.urdu_to_hindi_converter.translate(text)
-
         return text
--- indo_arabic_transliteration/utils.py
+++ indo_arabic_transliteration/utils.py
@@ -5,7 +5,7 @@ class StringTranslator:
     A re-implementation of str.maketrans() to support multi-letter keys.
     More details: https://stackoverflow.com/q/63230213
     '''
-    def __init__(self, translation_dict, sort_by_descending_key_length=True):
+    def __init__(self, translation_dict, sort_by_descending_key_length=True, match_initial_only=False, match_final_only=False):
 
         if sort_by_descending_key_length:
             self.translation_dict = {}
@@ -14,7 +14,12 @@ class StringTranslator:
         else:
             self.translation_dict = translation_dict
         
-        self.regex = re.compile('|'.join(map(re.escape, self.translation_dict)))
+        regex_str = '|'.join(map(re.escape, self.translation_dict))
+        if match_initial_only:
+            regex_str = r'\b' + regex_str.replace('|', r'|\b')
+        elif match_final_only:
+            regex_str = regex_str.replace('|', r'\b|') + r'\b'
+        self.regex = re.compile(regex_str)
 
     def translate(self, text):
         return self.regex.sub(lambda match: self.translation_dict[match.group(0)], text)
